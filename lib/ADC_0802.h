d814d92fff28510538ab073a1bd2098434aa0385
Gabriel Casarin da Silva <gabriel.casarin@hotmail.com>
2016-08-14 13:26:47 -0300

    corrigida a reutilização de mesmo nome ADC

--- lib/ADC_0802.h
+++ lib/ADC_0802.h
@@ -5,13 +5,14 @@
  *
  */
 
-#ifndef ADC
-#define ADC
+#ifndef ADC_H
+#define ADC_H
 
 #include <8051.h>
 #include "interrupcoes.h"
 
-#define ADC 0x2200 /* posicao de acionamento do processo de conversao */
+#define ADC_ADDR 0x2200 /* posicao de acionamento do processo de conversao */
+unsigned char __far __at (@ADC_ADDR) ADC;
 
 volatile unsigned char leu;
 volatile float temperatura_atual;
@@ -21,7 +22,7 @@ void init_adc0802() {
   IE = GLOBAL | EX1;
 }
 
-unsigned char __far __at 0xFFF6 vetorEX1 = {0x02, 0xA0, 0x13};
+unsigned char __far __at 0xFFF6 vetorEX1[] = {0x02, 0xA0, 0x13};
 void ext1_int() __interrupt 2 __using 1 {
   temperatura_atual = ADC;
   leu = 1;
