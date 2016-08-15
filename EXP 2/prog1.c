57e346e6e979bc136d8588fc6494150431b00a55
Kersul <joaohkfaria@gmail.com>
2016-08-15 10:13:58 -0300

    Update em sala

--- EXP 2/prog1.c
+++ EXP 2/prog1.c
@@ -1,6 +1,24 @@
 #include <8051.h>
+#include "terminal.h"
+#include "display_HD44780U.h"
 #include "ADC_0802.h"
 
 void main(void) {
+  unsigned int temp;
+
+  progrdisplay();
+  cleardisplay();  
+
   init_adc0802();
+    
+  while(1) {
+    temp = ler_temperatura();
+    	
+    CO(' ');
+    CHEX((char)temp);
+	
+	//cleardisplay();
+	//retorno_carro();
+	//displaydata(temp);
+  }
 }
