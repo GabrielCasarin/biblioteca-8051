41bc968716fd3ef63f289825ea346fd4466b523e
Kersul <joaohkfaria@gmail.com>
2016-08-15 10:14:57 -0300

    Update em sala

--- lib/terminal.h
+++ lib/terminal.h
@@ -20,6 +20,25 @@ char CI (void)
   return SBUF;
 }
 
+void CHEX (unsigned char dado)  {
+  unsigned char aux;
+  aux = dado/0x10; /* desloca 4 bits para a direita - dado AB: pega 0x0A */
+  if (aux<=0x09) { /* se A for número */
+    aux = aux + 0x30; /* converte número em ASCII */
+  }
+  else { /* se A for letra */
+    aux = aux + 0x37; /* converte letra em ASCII */
+  }
+  CO(aux); /* envia A em ASCII para o terminal de vídeo */
+  aux = dado&0x0F; /* dado AB: pega 0x0B */
+  if (aux<=0x09) /* se B for número */
+    aux = aux + 0x30; /* converte número em ASCII */
+  else /* se B for letra */
+    aux = aux + 0x37; /* converte letra em ASCII */
+  CO(aux); /* envia B em ASCII para o terminal de vídeo */
+
+}
+
 void print_terminal(char* cadeia, int len) {
   int i;
   for (i = 0; i < len; ++i) {
